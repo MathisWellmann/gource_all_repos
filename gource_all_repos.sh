0e701fe331f772b031d3a16a595e8406eff123d4
MathisWellmann <wellmannmathis@gmail.com>
2024-01-21 08:27:34 +0100

    Render in UHD

--- gource_all_repos.sh
+++ gource_all_repos.sh
@@ -1,12 +1,13 @@
-for dir in ./*/     # list directories in the form "/tmp/dirname/"
+for dir in ./*/
 do
-    dir=${dir%*/}      # remove the trailing "/"
-    echo "Calling gource on ${dir##*/}"    # print everything after the final "/"
+    dir=${dir%*/}
+    echo "Calling gource on ${dir##*/}"
     gource --output-custom-log gource_${dir##*/}.txt ./${dir##*/}
+    # Make every repo a distinct node in the final output
     sed -i -r "s#(.+)\|#\1|/${dir##*/}#" gource_${dir##*/}.txt
 done
 
 cat gource*.txt | sort -n > gource_combined.txt
 
-gource -1920x1080 -c 4 --max-user-speed 16 --dir-name-depth 3 --highlight-dirs -o - gource_combined.txt | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libvpx -b 30000K gource.webm
+gource -3840x2160 -c 4 -r 30 -max-user-speed 16 --dir-name-depth 3 --highlight-dirs -o - gource_combined.txt | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libvpx -b 100000K gource.webm
 
